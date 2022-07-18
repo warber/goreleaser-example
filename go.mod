eaddfbb6761a29eb0d9d6ff6136d4e8332667435
warber <bernd.warmuth@dynatrace.com>
2022-07-18 09:09:52 +0200

    changed goreleaser stuff
    Signed-off-by: warber <bernd.warmuth@dynatrace.com>

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/goreleaser/example
+module github.com/warber/goreleaser-example
 
-go 1.15
+go 1.18
---
+++ pkg/lib/lib_test.go
@@ -0,0 +1,11 @@
+package lib_test
+
+import (
+	"testing"
+
+	"github.com/warber/goreleaser-example/pkg/lib"
+)
+
+func Test_Hello(t *testing.T) {
+	lib.Hello()
+}
